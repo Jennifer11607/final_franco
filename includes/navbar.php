ef8e8c08dbd6bdd88ef4175a0d5a6283b20185d3
lestermaudiel <flestermaudiel2130@gmail.com>
2023-06-12 08:54:03 -0600

    navbar. creacion

---
+++ includes/navbar.php
@@ -0,0 +1,83 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Hospital La Esperanza</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Hospital</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Citas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/citas/index.php">Ingresar Citas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/citas/buscar.php">Buscar Citas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/citas/detalle.php">Detalle Citas</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Pacientes
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/pacientes/index.php">Ingresar Paciente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/pacientes/buscar.php">Buscar Paciente</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Medicos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/medicos/index.php">Ingresar Medico</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/medicos/buscar.php">Buscar Medico</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Clinicas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/clinicas/index.php">Ingresar clinica</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/clinicas/buscar.php">Buscar clinica</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Especialidades
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/especialidades/index.php">Ingresar especialidad</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_franco/vistas/especialidades/buscar.php">Buscar especialidad</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
