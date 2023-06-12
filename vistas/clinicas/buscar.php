afd347c5a3db6bcf10ecd3c4ca70880faa6f4d44
lestermaudiel <flestermaudiel2130@gmail.com>
2023-06-12 15:44:15 -0600

    se actualizo clinica

--- vistas/clinicas/buscar.php
+++ vistas/clinicas/buscar.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);?>
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Buscar Medicos</h1>
+        <h1 class="text-center">Buscar clinica</h1>
         <div class="row justify-content-center">
             <form action="/final_franco/controladores/clinicas/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
