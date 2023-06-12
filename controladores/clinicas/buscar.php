c3edd46f9ab8f5152d659f81eb8a61e9d39dad8b
lestermaudiel <flestermaudiel2130@gmail.com>
2023-06-12 05:23:29 -0600

    Creacion carpeta clinicas y creacion de archivo buscar.

--- controladores/clinicas/buscar.php
+++ controladores/clinicas/buscar.php
@@ -10,7 +10,6 @@ try {
 } catch (Exception $e2){
     $error = $e2->getMessage();
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -19,7 +18,7 @@ try {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Buscar Clinicas</title>
+    <title>BUSCAR CLINICAS.</title>
 </head>
 <body>
     <div class="container">
